998bbb77eda0ca1d369ee414a93d1644988dd797
Noah Gundotra <noah@gundotra.org>
2025-05-16 17:46:05 -0400

    update path from use-case to companies, fix github button

--- src/entities/home/ui/call-to-action.tsx
+++ src/entities/home/ui/call-to-action.tsx
@@ -6,10 +6,10 @@ import { Icon } from '@/shared/ui/icon'
 export const CallToAction: FC = () => {
     return (
         <Hero title="Start building on SAS today" subTitle="An infrastructure public good for making attestations onchain or offchain about anything.">
-            <Button href="https://github.com" variant="primary" icon={Icon.Github}>
+            <Button href="https://github.com/solana-foundation/solana-attestation-service" variant="primary" icon={Icon.Github}>
                 Explore on Github
             </Button>
-            <Button href="https://github.com" variant="secondary" icon={Icon.Documentation}>
+            <Button href="https://github.com/solana-foundation/solana-attestation-service" variant="secondary" icon={Icon.Documentation}>
                 Read documentation
             </Button>
         </Hero>
