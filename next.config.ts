34dd171ac6acd7fe8b00449422c22a90ec992ecb
pkxro <solponomarev@gmail.com>
2025-05-17 14:53:34 -0400

    init: docs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     /* config options here */
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    experimental: {
+        mdxRs: true,
+    },
+    basePath: '/docs',
+
+    async rewrites() {
+        return [
+            {
+                source: '/docs/:path*',
+                destination: '/docs/:path*',
+            },
+        ]
+    },
 }
 
 export default nextConfig
