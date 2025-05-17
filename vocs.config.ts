4d0ed34ab11be26846bce73d90b8c66f2e5bec27
pkxro <solponomarev@gmail.com>
2025-05-17 16:40:29 -0400

    nit: mvp impl of docs

--- vocs.config.ts
+++ vocs.config.ts
@@ -3,20 +3,48 @@ import { defineConfig } from 'vocs'
 export default defineConfig({
     title: 'SAS',
     description: 'Documentation for Solana Attestation Servie',
-    basePath: '/docs',
     font: {
         default: {
             google: 'Geist Mono',
         },
     },
     theme: {
-        colorScheme: 'dark'
+        colorScheme: 'light'
     },
+    iconUrl: {
+        light: '/logo.svg',
+        dark: '/logo.svg',
+      },
+      logoUrl: {
+        light: '/logo.svg',
+        dark: '/logo.svg',
+      },
     sidebar: [
         {
             text: 'Getting Started',
-            link: '/getting-started',
+            link: '/docs/getting-started',
         },
+        {
+            text: "Guides",
+            collapsed: false,
+            items: [
+                {
+                    text: "Creating a Credential",
+                    link: "/docs/guides/creating-a-credential",
+                },
+                {
+                    text: "Creating a Schema",
+                    link: "/docs/guides/creating-a-schema",
+                },
+                {
+                    text: "Creating an Attestation",
+                    link: "/docs/guides/creating-an-attestation",
+                },
+                {
+                    text: "Parsing an Attestation",
+                    link: "/docs/guides/parsing-an-attestation",
+                },
+            ],
+        }
     ],
-    iconUrl: '/logo.svg',
 })
