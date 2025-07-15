7713b35bd32a43b466d4fe241b3da6bbcca8066d
pkxro <solponomarev@gmail.com>
2025-07-15 15:18:22 -0400

    fix: fonts for site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,28 @@
 import type { Metadata } from 'next'
-import localFont, { Geist } from 'next/font/google'
+import localFont from 'next/font/local'
 import { FC, PropsWithChildren } from 'react'
 import { GoogleAnalytics } from '@/entities/settings'
 import './globals.css'
 
-const siteFont = Geist({ subsets: ['latin'] })
+const siteFont = localFont({
+    src: [
+        {
+            path: "../public/BerkeleyMono-Regular.otf",
+            style: "normal",
+            weight: "400"
+        },
+        {
+            path: "../public/BerkeleyMono-Bold.otf",
+            style: "bold",
+            weight: "700"
+        },
+        {
+            path: "../public/BerkeleyMono-Oblique.otf",
+            style: "italic",
+            weight: "400"
+        },
+    ]
+})
 
 export const metadata: Metadata = {
     title: 'Solana Attestation Service',
